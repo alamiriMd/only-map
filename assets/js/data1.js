719191cea3d6ac1a9618918a393b1ae81b2b3d09
Your Name <iraqiProfessionalDesigner@gmail.com>
2023-09-01 22:04:22 +0700

    first commit

---
+++ assets/js/data1.js
@@ -0,0 +1,47 @@
+export const coordinates = [
+	{
+		class: "blue",
+		text: "ЖД станция",
+		coordinates: { "x": 30.208, "y": 26.111 }
+	},
+	{
+		class: "blue",
+		text: "Главный вход",
+		coordinates: { "x": 23.229, "y": 66.111 }
+	},
+	{
+		class: "blue",
+		text: "Тренажерный зал",
+		coordinates: { "x": 56.354, "y": 58.426 }
+	},
+	{
+		class: "blue",
+		text: "Жилые районы",
+		coordinates: { "x": 71.302, "y": 20.833 }
+	},
+	{
+		class: "blue",
+		text: "Культурный центр",
+		coordinates: { "x": 73.646, "y": 43.981 }
+	},
+	{
+		class: "blue",
+		text: "Комплекс 2",
+		coordinates: { "x": 82.812, "y": 66.018 }
+	},
+	{
+		class: "green",
+		text: "Медицинский центр",
+		coordinates: { "x": 59.896, "y": 37.407 }
+	},
+	{
+		class: "green",
+		text: "Комплекс 1",
+		coordinates: { "x": 67.708, "y": 61.806 }
+	},
+	{
+		class: "green",
+		text: "Жилые комплексы",
+		coordinates: { "x": 57.552, "y": 72.685 }
+	}
+]
---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+import { coordinates } from "./data1.js";
+const block = document.querySelector(".block");
+const containerBtn = document.getElementById("_container");
+
+
+function firstLoadHandler() {
+	coordinates.forEach((data, index) => {
+		const buttonClone = containerBtn.cloneNode(true);
+		buttonClone.classList.add(data.class);
+		buttonClone.querySelector(".btnText").textContent = data.text;
+		buttonClone.id = `btn_${index}`;
+		buttonClone.style.left = data.coordinates.x + "%";
+		buttonClone.style.top = data.coordinates.y + "%";
+		block.appendChild(buttonClone);
+	});
+}
+
+block.addEventListener("click", (event) => {
+	const clickedButton = event.target.closest(".overlayBtn");
+	if (clickedButton) {
+		toggleButtonState(clickedButton);
+	} else {
+		const allButtons = block.querySelectorAll(".overlayBtn");
+		allButtons.forEach((button) => {
+			button.classList.remove("open");
+			const verticalLine = button.querySelector(".verticalLine");
+			const buttonText = button.querySelector(".btnText");
+			buttonText.classList.remove("hidden");
+			verticalLine.classList.remove("hidden");
+		});
+	}
+});
+
+function toggleButtonState(button) {
+	const isButtonOpen = button.classList.toggle("open");
+	const verticalLine = button.querySelector(".verticalLine");
+	const buttonText = button.querySelector(".btnText");
+
+	buttonText.classList.toggle("hidden", isButtonOpen);
+	verticalLine.classList.toggle("hidden", isButtonOpen);
+}
+
+document.addEventListener("DOMContentLoaded", firstLoadHandler);
